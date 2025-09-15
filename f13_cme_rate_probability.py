d5583b06f1f0f6b8957c634621722a0cc62adc09
jaeranglee <jrang.lee@gmail.com>
2025-09-15 22:17:58 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ f13_cme_rate_probability.py
@@ -0,0 +1,81 @@
+# [그림 13] 정책금리 기대확률(2025년 9월 17일 FOMC)
+
+# 이용되는 data는 CME Fed Watch 사이트에서 csv 파일로 다운로드한 것이다.
+# CME는 무료로 공개된 Open API는 없다. Fed Watch 사이트에 가면 일부 자료를 화면으로 보여주거나
+# csv 파일로 공개한다.
+
+# https://www.cmegroup.com/ko/markets/interest-rates/cme-fedwatch-tool.html
+# 이중에 historical 쪽 자료를 받은 것이다.
+
+# 저작권 문제로 csv 파일을 GitHub에 올리지 않았다.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import platform
+from datetime import datetime
+
+# 마이너스 기호 깨짐 방지
+plt.rcParams['axes.unicode_minus'] = False
+
+# 폰트 설정
+if platform.system() == 'Darwin':
+    plt.rcParams['font.family'] = 'AppleGothic'
+else:
+    plt.rcParams['font.family'] = 'Malgun Gothic'
+
+
+df_pro = pd.read_csv('data/FedMeeting_20250917.csv')
+df_pro['Date'] = pd.to_datetime(df_pro['Date'])
+
+# 현재 날짜 가져오기
+access_date = datetime.today().strftime('%Y-%m-%d')
+
+# 시각화
+fig, ax = plt.subplots(figsize=(8, 5), constrained_layout=True)
+
+
+ax.plot(df_pro['Date'], df_pro['(375-400)']*100, label='50bp 인하(3.75-4.00%)', ls='--', lw=1, color='green')
+ax.plot(df_pro['Date'], df_pro['(400-425)']*100, label='25bp 인하(4.00-4.25%)', lw=2, color='red')
+ax.plot(df_pro['Date'], df_pro['(425-450)']*100, label='동결(4.25-4.5%)', lw=1, color='blue')
+
+
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+
+ax.text(0, 1.00, '(%)', ha='left', va='bottom',transform=ax.transAxes, fontsize=12, color='black')
+ax.legend(loc='upper left',bbox_to_anchor=(0.0, 1))
+
+ax.text(0, -0.15, f'출처: CME FedWatch, 자료 작성일: {access_date}', transform=ax.transAxes, fontsize=10)
+ax.grid(True, linestyle='--', color='gray', alpha=0.3)
+
+
+# 저장 및 출력
+# python py 파일 이름과 동일한 이름으로 그림 파일을 tif, jpg로 저장하는 루틴
+import os
+from PIL import Image
+
+
+## 현재 작업 중인 파일 이름 추출 (확장자 제거)
+
+try:
+    base_filename = os.path.splitext(os.path.basename(__file__))[0]
+except NameError:
+    base_filename = "default_filename"
+
+# 저장 경로 설정
+image_path_tif = f"pic_tif/{base_filename}.tif"
+image_path_jpg = f"pic_jpg/{base_filename}.jpg"
+
+# 디렉토리 없으면 자동 생성
+os.makedirs("pic_tif", exist_ok=True)
+os.makedirs("pic_jpg", exist_ok=True)
+
+# 그래프 저장
+plt.savefig(image_path_tif, dpi=300)
+plt.savefig(image_path_jpg, dpi=300)
+
+# JPEG → CMYK 변환 후 덮어쓰기
+img = Image.open(image_path_jpg).convert("CMYK")
+img.save(image_path_jpg, "JPEG")
+plt.show()
